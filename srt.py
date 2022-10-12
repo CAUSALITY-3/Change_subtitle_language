f6fae919c27153c0618d3a5b3edd02250a3a7aa7
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2022-10-12 20:12:16 +0530

    Create srt.py

---
+++ srt.py
@@ -0,0 +1,26 @@
+from googletrans import Translator
+
+translator = Translator()
+
+# provide file name with location 
+file1 = open('input_file.srt', 'r',encoding="utf-8")  
+Lines = file1.readlines()
+file1.close()
+# provide output file name with location 
+file2 = open('output_file.srt', 'w', encoding="utf-8")
+for i in Lines:
+    
+    try:
+        try:
+            if (type(int(i))==int):
+                file2.write(i)
+        except:
+            if ('-->' in i):
+                file2.write(i)
+            else:
+                translated = translator.translate(i, src='en', dest='ml') #source language = en (English) and destination/output language = ml (malayalam)
+                file2.write(f"{translated.text} \n")
+    except:
+        file2.write(i)
+
+file2.close()
